c0c3997afc5529084fa33b6155219ad69ec80a81
Marten Pieper <marten.l.pieper@gmail.com>
2019-01-21 18:45:28 +0100

    Formatted and cleaned index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require("./config");
 app.use(express.static("./public"));
 const s3 = require("./s3");
 
-// Boilerplate code  Do Not Touch!
+// Boilerplate code - Do not change
 var multer = require("multer");
 var uidSafe = require("uid-safe");
 var path = require("path");
@@ -32,10 +32,7 @@ var uploader = multer({
 });
 
 app.post("/upload", uploader.single("file"), s3.upload, function(req, res) {
-    // If nothing went wrong the file is already in the uploads directory
     let s3Url = config.s3Url + req.file.filename;
-    // console.log("s3.upload", s3.upload);
-    // console.log("s3Url", s3Url);
     if (req.file) {
         db.uploadImages(
             s3Url,
@@ -44,7 +41,6 @@ app.post("/upload", uploader.single("file"), s3.upload, function(req, res) {
             req.body.username
         )
             .then(function(results) {
-                // console.log("results in app.post.then", results);
                 res.json({
                     data: results
                 });
@@ -52,8 +48,6 @@ app.post("/upload", uploader.single("file"), s3.upload, function(req, res) {
             .catch(function(err) {
                 console.log("error in app.post", err);
             }); //file,description,title, username
-        // console.log("req.file", req.file);
-        // console.log("req.body", req.body);
     } else {
         res.json({
             success: false
@@ -65,7 +59,6 @@ app.post("/upload", uploader.single("file"), s3.upload, function(req, res) {
 app.get("/get-images", (req, res) => {
     db.getImages()
         .then(function(results) {
-            // console.log("results", results);
             res.json(results.rows);
         })
         .catch(function(err) {
@@ -76,57 +69,47 @@ app.get("/get-images", (req, res) => {
 app.get("/image/:id", (req, res) => {
     db.getImageById(req.params.id)
         .then(function(results) {
-            // console.log("results", results);
             res.json(results);
         })
         .catch(function(err) {
-            console.log(err);
+            console.log("error in app.get /image/:id", err);
         });
 });
 
 app.post("/comments", function(req, res) {
-    console.log("req in app.post /comments", req.body);
-
     db.uploadComments(req.body.comment, req.body.username, req.body.imageId)
         .then(function(results) {
-            // console.log("results in app.post.then", results);
             res.json(results.rows);
         })
         .catch(function(err) {
-            console.log("error in app.post", err);
+            console.log("error in app.post /comments", err);
         });
 });
 
 app.get("/get-more-images/:id", (req, res) => {
-    // console.log("req.params.id", req.params.id);
     var lastId = req.params.id;
     db.getMoreImages(lastId).then(images => {
-        // console.log("images in the get more images", images);
         res.json(images);
     });
 });
 
 app.post("/completed-Component", (req, res) => {
-    console.log("req in app.post /completed-Component", req.body);
     db.updateCompletion(req.body.imageId, req.body.completedComponent)
         .then(function(results) {
-            console.log("results in /completed-Component", results);
             res.json(results);
         })
         .catch(function(err) {
-            console.log("error in app.post", err);
+            console.log("error in app.post /completed-Component", err);
         });
 });
 
 app.get("/components-completed", (req, res) => {
-    console.log("req in app.get /component-completed", req.body);
     db.getCompletion()
         .then(function(results) {
-            console.log("results in app.get /component-completed:", results);
             res.json(results);
         })
         .catch(function(err) {
-            console.log("error in app.post", err);
+            console.log("error in app.post /components-completed", err);
         });
 });
 
